4d397945a15d82de2c8f1f7156d3682c08a02b04
Raimond <37875805+bogdiun@users.noreply.github.com>
2024-07-11 14:37:23 +0300

    configure swagger  (#2)
    * initial swagger config
    
    * Add api versioning and swagger documentation
    
    * Add api versioning with swagger

---
+++ NoteServiceDemo/src/Controllers/NotesController.cs
@@ -0,0 +1,82 @@
+﻿namespace NotesService.API.Controllers;
+
+using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/v{v:apiVersion}/[controller]")]
+[ApiVersion("1.0")]
+[Produces("application/json")]
+public class NotesController : ControllerBase
+{
+    private readonly ILogger<NotesController> _logger;
+
+    public NotesController(ILogger<NotesController> logger)
+    {
+        _logger = logger;
+    }
+
+    /// <summary>
+    /// Gets all notes for the user
+    /// </summary>
+    /// <returns>id's and descriptions of all notes</returns>
+    /// <exception cref="NotImplementedException"></exception>
+    [HttpGet]
+    // TODO: Add [ProducesResponseType(200, typeof(ExampleTypeObject))]
+    public async Task<IActionResult> GetAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Gets note with specified id.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>Note details</returns>
+    /// <exception cref="NotImplementedException"></exception>
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetAsync(int id)
+    {
+        // TODO: change id to from query?
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Posts a note in the body for storage
+    /// </summary>
+    /// <param name="body">Note body</param>
+    /// <returns>Note id</returns>
+    /// <exception cref="NotImplementedException"></exception>
+    [HttpPost()]
+    public async Task<IActionResult> PostAsync([FromBody] string body)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Updates the specified note (by id)
+    /// </summary>
+    /// <param name="id">id of a note to update</param>
+    /// <param name="body">new note details</param>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutAsync(int id, [FromBody] string body)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Deletes the specified note
+    /// </summary>
+    /// <param name="id">id of the note to delete</param>
+    /// <returns>details of a deleted note?</returns>
+    /// <exception cref="NotImplementedException"></exception>
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    //[HttpPatch]
+}
--- NoteServiceDemo/src/Program.cs
+++ NoteServiceDemo/src/Program.cs
@@ -1,18 +1,52 @@
+using System.Reflection;
+using Asp.Versioning;
+using Microsoft.Extensions.Options;
+using NotesService.API.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddApiVersioning(c =>
+                {
+                    c.DefaultApiVersion = new ApiVersion(1, 0);
+                    c.AssumeDefaultVersionWhenUnspecified = true;
+                    c.ReportApiVersions = true;
+                })
+                .AddMvc().AddApiExplorer(c =>
+                {
+                    c.GroupNameFormat = "'v'VVV";
+                    c.SubstituteApiVersionInUrl = true;
+                });
+
+builder.Services.AddSwaggerGen(c =>
+{
+    string xmlDocFilename = Path.Combine(AppContext.BaseDirectory, Assembly.GetExecutingAssembly().GetName().Name + ".xml");
+    c.IncludeXmlComments(xmlDocFilename);
+    c.OperationFilter<SwaggerDefaultValues>();
+    // TODO: Add Security at some point later | AddSecurityDefinition/Requirement
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
+    //TODO: might allow in production, add: c.RoutePrefix = "api/documentation"
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        foreach (var description in app.DescribeApiVersions())
+        {
+            c.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName);
+            c.DisplayRequestDuration();
+        }
+    });
 }
 
 app.UseHttpsRedirection();
@@ -22,3 +56,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+// TODO: hosting https://learn.microsoft.com/en-us/aspnet/core/host-and-deploy/?view=aspnetcore-2.1&tabs=aspnetcore2x
