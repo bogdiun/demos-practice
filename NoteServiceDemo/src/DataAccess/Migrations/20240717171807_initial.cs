91dac26ad19ab59b566f9f35261b73e13d258296
Raimond <37875805+bogdiun@users.noreply.github.com>
2024-07-17 19:21:20 +0200

    change ef to use localdb (sql server)

---
+++ NoteServiceDemo/src/DataAccess/Migrations/20240717171807_initial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NotesService.API.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NoteKey = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NoteValue = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastAccess = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    MediaType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Categories = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notes");
+        }
+    }
+}
